f2f5b1beea8296fd0c1784ede9a39f0614829985
Fahad <fahad.45843@iqra.edu.pk>
2024-09-29 01:49:11 +0500

    Urdu font

--- src/app/familyTree.tsx
+++ src/app/familyTree.tsx
@@ -5,55 +5,55 @@ export interface FamilyMember {
 }
 
 export const familyTree: FamilyMember = {
-    name: 'Jan Muhammad',
+    name: 'جان محمد',
     children: [
         {
-            name: 'Mullah Murad Sister',
+            name: 'ملا مراد کی بہن',
             children: [
                 {
-                    name: 'Asmat',
+                    name: 'عصمت',
                     children: [
                         {
-                            name: 'Amir',
+                            name: 'عامر',
                             children: [
-                                { name: 'Umar' },
-                                { name: 'Umar sister' },
-                                { name: 'Umair' },
+                                { name: 'عمر' },
+                                { name: 'عمر کی بہن' },
+                                { name: 'عمیر' },
                             ],
                         },
-                        { name: 'Tahir' },
-                        { name: 'Ali' },
-                        { name: 'Ali sister' },
-                        { name: 'Bilal' },
+                        { name: 'طاہر' },
+                        { name: 'علی' },
+                        { name: 'علی کی بہن' },
+                        { name: 'بلال' },
                     ],
                 },
-                { name: 'Mrs Nasir' },
-                { name: 'Ghulam' },
-                { name: 'Usman' },
-                { name: 'Mrs Naseer' },
-                { name: 'Jamil' },
+                { name: 'مسز ناصر' },
+                { name: 'غلام' },
+                { name: 'عثمان' },
+                { name: 'مسز نصیر' },
+                { name: 'جمیل' },
             ],
         },
         {
-            name: 'Mullah Murad',
+            name: 'ملا مراد',
             children: [
-                { name: 'Mrs. Haider' },
-                { name: 'Mrs. Bakhshi' },
-                { name: 'Mrs. Hanif' },
-                { name: 'Mrs. Khalid' },
-                { name: 'Mrs. Naeem' },
-                { name: 'Babul' },
-                { name: 'Mrs. Suhail' },
-                { name: 'Babul\'s sister' },
+                { name: 'مسز حیدر' },
+                { name: 'مسز بخشیش' },
+                { name: 'مسز حنیف' },
+                { name: 'مسز خالد' },
+                { name: 'مسز نعیم' },
+                { name: 'بابو' },
+                { name: 'مسز سہیل' },
+                { name: 'بابو کی بہن' },
             ],
         },
-        { name: 'Shahmeer' },
-        { name: 'Mauldad' },
-        { name: 'Ayoub' },
-        { name: 'Mrs. Abdullah' },
-        { name: 'Mrs. Karim' },
-        { name: 'Mrs. Ismail' },
-        { name: 'Anwar' },
+        { name: 'شاہ میر' },
+        { name: 'مولداد' },
+        { name: 'ایوب' },
+        { name: 'مسز عبداللہ' },
+        { name: 'مسز کریم' },
+        { name: 'مسز اسماعیل' },
+        { name: 'انوار' },
     ],
 };
 
@@ -62,6 +62,64 @@ export const familyTree: FamilyMember = {
 
 
 
+// // app/familyTree.ts
+// export interface FamilyMember {
+//     name: string;
+//     children?: FamilyMember[];
+// }
+
+// export const familyTree: FamilyMember = {
+//     name: 'Jan Muhammad',
+//     children: [
+//         {
+//             name: 'Mullah Murad Sister',
+//             children: [
+//                 {
+//                     name: 'Asmat',
+//                     children: [
+//                         {
+//                             name: 'Amir',
+//                             children: [
+//                                 { name: 'Umar' },
+//                                 { name: 'Umar sister' },
+//                                 { name: 'Umair' },
+//                             ],
+//                         },
+//                         { name: 'Tahir' },
+//                         { name: 'Ali' },
+//                         { name: 'Ali sister' },
+//                         { name: 'Bilal' },
+//                     ],
+//                 },
+//                 { name: 'Mrs Nasir' },
+//                 { name: 'Ghulam' },
+//                 { name: 'Usman' },
+//                 { name: 'Mrs Naseer' },
+//                 { name: 'Jamil' },
+//             ],
+//         },
+//         {
+//             name: 'Mullah Murad',
+//             children: [
+//                 { name: 'Mrs. Haider' },
+//                 { name: 'Mrs. Bakhshi' },
+//                 { name: 'Mrs. Hanif' },
+//                 { name: 'Mrs. Khalid' },
+//                 { name: 'Mrs. Naeem' },
+//                 { name: 'Babul' },
+//                 { name: 'Mrs. Suhail' },
+//                 { name: 'Babul\'s sister' },
+//             ],
+//         },
+//         { name: 'Shahmeer' },
+//         { name: 'Mauldad' },
+//         { name: 'Ayoub' },
+//         { name: 'Mrs. Abdullah' },
+//         { name: 'Mrs. Karim' },
+//         { name: 'Mrs. Ismail' },
+//         { name: 'Anwar' },
+//     ],
+// };
 
 
 
@@ -69,23 +127,73 @@ export const familyTree: FamilyMember = {
 
 
 
+
+
+
+
+
+
+// // // export interface FamilyMember {
+// // //     name: string;
+// // //     children?: FamilyMember[];
+// // // }
+
+// // // export const familyData: FamilyMember = {
+// // //     name: 'Haji Ismael',
+// // //     children: [
+// // //         {
+// // //             name: 'Abdul Qayyum',
+// // //             children: [
+// // //                 { name: 'Fahad' },
+// // //                 { name: 'Fahad\'s sister' },
+// // //                 { name: 'Ahmed' },
+// // //                 { name: 'Asad' },
+// // //                 { name: 'Asad\'s sister' },
+// // //                 { name: 'Ahmed\'s sister' },
+// // //             ],
+// // //         },
+// // //         { name: 'Abdul Rauf' },
+// // //         { name: 'Abdul Naeem' },
+// // //         { name: 'Mrs. Tariq' },
+// // //         { name: 'Ibrahim' },
+// // //         { name: 'Nadeem' },
+// // //         { name: 'Salman' },
+// // //         { name: 'Sohail' },
+// // //         { name: 'Saud' },
+// // //     ],
+// // // };
+
+
+
+
+// // // app/familyTree.ts
 // // export interface FamilyMember {
 // //     name: string;
 // //     children?: FamilyMember[];
 // // }
 
-// // export const familyData: FamilyMember = {
+// // export const familyTree: FamilyMember = {
 // //     name: 'Haji Ismael',
 // //     children: [
 // //         {
 // //             name: 'Abdul Qayyum',
 // //             children: [
-// //                 { name: 'Fahad' },
-// //                 { name: 'Fahad\'s sister' },
+// //                 {
+// //                     name: 'Fahad',
+// //                     children: [
+// //                         { name: 'Fahad' },
+// //                         { name: 'Fahad’s sister' },
+// //                         { name: 'Ahmed' },
+// //                         { name: 'Asad' },
+// //                         { name: 'Asad’s sister' },
+// //                         { name: 'Ahmed’s sister' },
+// //                     ],
+// //                 },
+// //                 { name: 'Fahad’s sister' },
 // //                 { name: 'Ahmed' },
 // //                 { name: 'Asad' },
-// //                 { name: 'Asad\'s sister' },
-// //                 { name: 'Ahmed\'s sister' },
+// //                 { name: 'Asad’s sister' },
+// //                 { name: 'Ahmed’s sister' },
 // //             ],
 // //         },
 // //         { name: 'Abdul Rauf' },
@@ -98,47 +206,3 @@ export const familyTree: FamilyMember = {
 // //         { name: 'Saud' },
 // //     ],
 // // };
-
-
-
-
-// // app/familyTree.ts
-// export interface FamilyMember {
-//     name: string;
-//     children?: FamilyMember[];
-// }
-
-// export const familyTree: FamilyMember = {
-//     name: 'Haji Ismael',
-//     children: [
-//         {
-//             name: 'Abdul Qayyum',
-//             children: [
-//                 {
-//                     name: 'Fahad',
-//                     children: [
-//                         { name: 'Fahad' },
-//                         { name: 'Fahad’s sister' },
-//                         { name: 'Ahmed' },
-//                         { name: 'Asad' },
-//                         { name: 'Asad’s sister' },
-//                         { name: 'Ahmed’s sister' },
-//                     ],
-//                 },
-//                 { name: 'Fahad’s sister' },
-//                 { name: 'Ahmed' },
-//                 { name: 'Asad' },
-//                 { name: 'Asad’s sister' },
-//                 { name: 'Ahmed’s sister' },
-//             ],
-//         },
-//         { name: 'Abdul Rauf' },
-//         { name: 'Abdul Naeem' },
-//         { name: 'Mrs. Tariq' },
-//         { name: 'Ibrahim' },
-//         { name: 'Nadeem' },
-//         { name: 'Salman' },
-//         { name: 'Sohail' },
-//         { name: 'Saud' },
-//     ],
-// };
