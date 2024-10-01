1d64223dbee7e7527e3c48988b9f2efcd47c4c5e
Fahad <fahad.45843@iqra.edu.pk>
2024-10-02 01:34:39 +0500

    Over clicking the services, quantity and prices with total dynamically updates

--- src/app/popup/page.tsx
+++ src/app/popup/page.tsx
@@ -5,6 +5,7 @@ import { RootState } from '../../lib/store'; // Import the RootState type
 import { incrementt, decrement } from '../../lib/features/counter/counterSlice';
 
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const Popup = () => {
 
@@ -14,7 +15,25 @@ const Popup = () => {
     // Optional: Use dispatch if you want to modify the count
     const dispatch = useDispatch();
 
+    let prices: number[] = [];
+    // let pricee: number = 0;
+    const [pricee, setPricee] = useState(0)
+
     const router = useRouter();
+
+    useEffect(() => {
+        { console.log('obj.productName...', obj && obj.productName) }
+        // let pricee = obj && obj.productName.reduce((a: any, b: any) => b.price + a.price, 0);
+        // let pricee = obj && obj.productName.reduce((a: any, b: any) => +b.price + +a.price, 0);
+        // let pricee = obj && obj.productName.map((v: any) => v.reduce((a: any, b: any) => a.price + b.price, 0));
+        obj && obj.productName.map((v: any) => prices.push(v.price));
+        let priceed = prices.reduce((a, b) => a + b, 0)
+        setPricee(priceed)
+        // let pricee = obj && obj.productName.reduce((a: any, b: any) => a.price);
+        // let pricee = obj && obj.productName.map((v: any) => v.price)
+        console.log('priceee...', pricee);
+    }, [obj])
+
     return (
         <div
             // className='absolute bg-red-500 right-8 bottom-1 p-2 rounded-lg'
@@ -23,7 +42,7 @@ const Popup = () => {
             <button
                 onClick={() => router.push('/calendars')}
             >
-                {obj && obj.productName.length}
+                {obj && obj.productName.length + ' - ' + '$' + pricee}
             </button>
         </div>
     )
