dd86302a75c23c03403d8e68dd4f1ff156dbb66d
Fahad <fahad.45843@iqra.edu.pk>
2024-10-03 00:24:35 +0500

    Popup with Collapser halfly configured

--- src/app/popup/page.tsx
+++ src/app/popup/page.tsx
@@ -1,5 +1,11 @@
 "use client"
 
+import React from 'react';
+import type { CollapseProps } from 'antd';
+import { Collapse } from 'antd';
+
+import { DeleteOutlined } from '@ant-design/icons';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../lib/store'; // Import the RootState type
 import { incrementt, decrement } from '../../lib/features/counter/counterSlice';
@@ -7,7 +13,23 @@ import { incrementt, decrement } from '../../lib/features/counter/counterSlice';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
-const Popup = () => {
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+const items: CollapseProps['items'] = [
+    {
+        key: '1',
+        label: 'pricee',
+        children: <p>{text}</p>,
+        //                 {obj && obj.productName.length + ' - ' + '$' + pricee}
+
+    }
+];
+
+const Popup: React.FC = () => {
 
     // const count = useSelector((state: RootState) => state.counter?.value);
     const obj = useSelector((state: RootState) => state.counter?.obj);
@@ -17,12 +39,21 @@ const Popup = () => {
 
     let prices: number[] = [];
     // let pricee: number = 0;
-    const [pricee, setPricee] = useState(0)
+
+    type productType = {
+        name: string,
+        price: number,
+        imageUrl: string,
+    }
+
+    const [pricee, setPricee] = useState(0);
+    const [products, setProducts] = useState<(productType | string)[]>([]);
 
     const router = useRouter();
 
     useEffect(() => {
         { console.log('obj.productName...', obj && obj.productName) }
+        obj && setProducts([...obj.productName])
         // let pricee = obj && obj.productName.reduce((a: any, b: any) => b.price + a.price, 0);
         // let pricee = obj && obj.productName.reduce((a: any, b: any) => +b.price + +a.price, 0);
         // let pricee = obj && obj.productName.map((v: any) => v.reduce((a: any, b: any) => a.price + b.price, 0));
@@ -34,18 +65,90 @@ const Popup = () => {
         console.log('priceee...', pricee);
     }, [obj])
 
-    return (
-        <div
-            // className='absolute bg-red-500 right-8 bottom-1 p-2 rounded-lg'
-            className='fixed bg-red-500 right-8 bottom-6 p-2 rounded-lg active:scale-105'
-        >
-            <button
-                onClick={() => router.push('/calendars')}
-            >
-                {obj && obj.productName.length + ' - ' + '$' + pricee}
-            </button>
-        </div>
-    )
-}
+    const items: CollapseProps['items'] = [
+        {
+            key: '1',
+            // label: pricee,
+            label:
+                obj && obj.productName.length + ' - ' + '$' + pricee,
+            // children: <p>{text}</p>,
+            // children: <p>{obj && obj.productName.map(v => v)}</p>,
+            children: <div>{products.map((v, i) => typeof v === 'object' && 'name' in v ? <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between'
+                }}
+            ><span>{i + 1 + ' - ' + v.name}</span><span><DeleteOutlined /></span></div> : v)}</div>,
+            //                 {obj && obj.productName.length + ' - ' + '$' + pricee}
 
-export default Popup
+        }
+    ];
+
+    const onChange = (key: string | string[]) => {
+        console.log(key);
+    };
+
+    return <Collapse items={items} defaultActiveKey={['1']} onChange={onChange}
+        style={{
+            backgroundColor: 'white'
+        }}
+    />;
+};
+
+export default Popup;
+
+
+
+
+
+// "use client"
+
+// import { useSelector, useDispatch } from 'react-redux';
+// import { RootState } from '../../lib/store'; // Import the RootState type
+// import { incrementt, decrement } from '../../lib/features/counter/counterSlice';
+
+// import { useRouter } from 'next/navigation';
+// import { useEffect, useState } from 'react';
+
+// const Popup = () => {
+
+//     // const count = useSelector((state: RootState) => state.counter?.value);
+//     const obj = useSelector((state: RootState) => state.counter?.obj);
+
+//     // Optional: Use dispatch if you want to modify the count
+//     const dispatch = useDispatch();
+
+//     let prices: number[] = [];
+//     // let pricee: number = 0;
+//     const [pricee, setPricee] = useState(0)
+
+//     const router = useRouter();
+
+//     useEffect(() => {
+//         { console.log('obj.productName...', obj && obj.productName) }
+//         // let pricee = obj && obj.productName.reduce((a: any, b: any) => b.price + a.price, 0);
+//         // let pricee = obj && obj.productName.reduce((a: any, b: any) => +b.price + +a.price, 0);
+//         // let pricee = obj && obj.productName.map((v: any) => v.reduce((a: any, b: any) => a.price + b.price, 0));
+//         obj && obj.productName.map((v: any) => prices.push(v.price));
+//         let priceed = prices.reduce((a, b) => a + b, 0)
+//         setPricee(priceed)
+//         // let pricee = obj && obj.productName.reduce((a: any, b: any) => a.price);
+//         // let pricee = obj && obj.productName.map((v: any) => v.price)
+//         console.log('priceee...', pricee);
+//     }, [obj])
+
+//     return (
+//         <div
+//             // className='absolute bg-red-500 right-8 bottom-1 p-2 rounded-lg'
+//             className='fixed bg-red-500 right-8 bottom-6 p-2 rounded-lg active:scale-105'
+//         >
+//             <button
+//                 onClick={() => router.push('/calendars')}
+//             >
+//                 {obj && obj.productName.length + ' - ' + '$' + pricee}
+//             </button>
+//         </div>
+//     )
+// }
+
+// export default Popup
